8724ff063918c8427505faf159440d79aa7d5c92
ariel3259 <arielsantangelo610@gmail.com>
2022-04-04 12:13:44 -0300

    Inciso 3 del primer día del challenge subido

---
+++ 1/Promedio.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Promedio{
+
+    public static void main(String[] args){
+        double nota1, nota2, nota3;
+        double promedio;
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Ingrese la primera nota:");
+        nota1 = scan.nextDouble();
+
+        System.out.println("Ingrese la segunda nota:");
+        nota2 = scan.nextDouble();
+
+        System.out.println("Ingrese la tercera nota:");
+        nota3 = scan.nextDouble();
+
+        promedio = (nota1 + nota2 + nota3)/3;
+
+        if(promedio < 10) System.out.println("Reprobado");
+        else if((promedio > 10) && (promedio < 15)) System.out.println("Aprobado");
+        else if(promedio > 15) System.out.println("Eximido");
+ 
+    }
+}
